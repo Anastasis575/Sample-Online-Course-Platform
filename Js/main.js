cb7369e67bae426705d4c8a0a8631d7db329e6e4
Anastasis575 <79604688+Anastasis575@users.noreply.github.com>
2022-01-04 19:15:20 +0200

    finished fucntionality for use case 1

--- Js/main.js
+++ Js/main.js
@@ -1,33 +1,95 @@
-var available=[]
-
+var url;
 
 window.onload=function () {
-    url="https://elearning-aueb.herokuapp.com/"
-    document.getElementById("search").onclick=getT;
+    url="https://elearning-aueb.herokuapp.com/";
+    getInitial(url);
+    document.getElementById("search").onclick=searchLesson;
 }
 
-
-function getT() {
-    var myHead=new Headers();
+//
+function searchLesson(){
+    //GET request
+    let myHead=new Headers();
     myHead.append("Accept","application/json");
-    var init={
+    let text=document.getElementById("SearchInput");
+    let dns;
+    if(text){
+        dns=document.getElementById("SearchInput").value;
+    }else{
+        console.log("Error text input not find");
+    }
+    let init={
         method: "GET",
         headers: myHead
     };
-    fetch(url+"categories/",init).then(obj=>obj.json())
-    .then(fin=>{
-        var text="";
-        fin.forEach(el=>{
-            text+=el.id+", "+el.title+"\n";
+    fetch(url+`courses/search?title=${dns.replace(" ","%20")}`,init)
+//  Turning the results to JSON
+    .then(res=>res.json())
+//   Generating the object data for the Class sections
+    .then(obj=>{
+        document.getElementById("Class-Data").innerHTML="";
+        var data=[];
+        obj.forEach(el=>{
+            // console.log(el["category"]);
+            let o=el;
+            o.img_dest=`https://elearning-aueb.herokuapp.com/static/images/${el.img}`;
+            o.img_alt=el.title+"_image";
+            data.push(o);
         })
-        document.getElementById("hello").innerHTML=text;
+        loadEntries(data);
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+}
+
+// Initial Fetch Request to get Categories
+function getInitial(url) {
+    let myHead=new Headers();
+    myHead.append("Accept","application/json");
+    let init={
+        method: "GET",
+        headers: myHead
+    };
+    fetch(url+"categories/",init)
+    .then(obj=>obj.json())
+    .then(fin=>{
+        console.log("Loaded");
+        var data=[];
+        fin.forEach(el=>{
+            let o=el;
+            o.link=`contents.html?category=${o.id}`;
+            data.push(o);
+        })
+        loadMenu(data);
     })
     .catch(err=>{console.log(err)})
 }
+
+
+// Loading the handlebars template and loading the content
+function loadEntries(data) {
+    let template=document.getElementById("lesson-template").innerHTML;
+    let compiled_template=Handlebars.compile(template);
+    let finall=compiled_template({classes: data});
+    let sec=document.getElementById("Class-Data");
+    sec.innerHTML=finall;
+}
+
+
+function loadMenu(data) {
+    let template=document.getElementById("menu-template").innerHTML;
+    let compiled_template=Handlebars.compile(template);
+    let finall=compiled_template({categories: data});
+    let sec=document.getElementById("Menu");
+    sec.innerHTML=finall;
+}
+
+// Event listener for Enter events
 window.addEventListener("keyup",event=>{
     if(event.key==="Enter"){
         if(document.getElementById("SearchInput")===document.activeElement){
-            // getT();
+            searchLesson();
         }
     }
 })
